c85476e4046d4449ab75df961d4de438ce68d29c
oliverjam <oliverjamesphillips@gmail.com>
2021-01-08 16:54:32 +0000

    Redesign nav
    Now made up of collapsible sections

--- assets/nav.js
+++ assets/nav.js
@@ -1,19 +1,8 @@
-const main = document.querySelector("#main");
-const globalNav = document.querySelector("#globalNav");
-const toggleNav = document.querySelector("#toggleNav");
+const toggle = document.querySelector("#toggle-nav");
 
-const closeNav = () => {
-  globalNav.classList.remove("active");
-};
-
-toggleNav.addEventListener("click", () => {
-  globalNav.classList.toggle("active");
-  if (globalNav.classList.contains("active")) {
-    main.addEventListener("click", closeNav, { once: true });
-    document.addEventListener(
-      "keydown",
-      (event) => event.key === "Escape" && closeNav(),
-      { once: true }
-    );
-  }
+toggle.hidden = false;
+toggle.setAttribute("aria-expanded", false);
+toggle.addEventListener("click", () => {
+  const expanded = toggle.getAttribute("aria-expanded") === "true" || false;
+  toggle.setAttribute("aria-expanded", !expanded);
 });
