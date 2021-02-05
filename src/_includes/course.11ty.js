bc7a380b90a6cdd4a0e5f08f86786f9b7a26ebcc
oliverjam <oliverjamesphillips@gmail.com>
2021-02-05 12:53:02 +0000

    Add installation guides to sidebar nav

--- src/_includes/course.11ty.js
+++ src/_includes/course.11ty.js
@@ -20,6 +20,8 @@ exports.render = ({ page: { url }, content }) => {
         <${Link} url=${url} href="/course/introduction">Introduction</${Link}>
         <${Link} url=${url} href="/course/code-of-conduct/">Code of Conduct</${Link}>
         <${Section} title="Handbook" url=${url} subpath="course/handbook">
+          <li><${Link} url=${url} href="/course/handbook/system-requirements/">System Requirements</${Link}></li>
+          <li><${Link} url=${url} href="/course/handbook/installation/">Installation Guide</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/course-rules/">Course Rules</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/pair-programming/">Pair programming</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/debugging/">Debugging</${Link}></li>
