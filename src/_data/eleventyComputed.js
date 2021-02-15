a5cff3db7bd151b3036471643c5f6ac8d01c1a4a
oliverjam <oliverjamesphillips@gmail.com>
2021-02-15 17:23:02 +0000

    Move description of curriculum to published /about

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,15 +1,4 @@
 module.exports = {
-  permalink: (data) => {
-    // README.md should always be the homepage
-    // this is so we can avoid having frontmatter in the readme
-    // since this would show up on GitHub and look weird
-    // https://www.11ty.dev/docs/data-computed/
-    if (data.page.fileSlug === "README") {
-      return "/about/index.html";
-    } else {
-      return data.permalink;
-    }
-  },
   layout: (data) => {
     if (data.page.url.includes("starter-files")) return false;
     if (data.layout) return data.layout;
