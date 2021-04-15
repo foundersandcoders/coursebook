ca8bfad7d11e9f9f2714a5182af8f7adb2f77fa3
oliverjam <oliverjamesphillips@gmail.com>
2021-04-15 12:17:16 +0100

    Improve solution block design

--- src/_includes/shortcodes/index.js
+++ src/_includes/shortcodes/index.js
@@ -1,9 +1,11 @@
 function solution(content) {
-  return /*html*/ `
-<details class="disclosure flow">
-  <summary class="button">Toggle answer</summary>
-  ${content}
-</details>
+  return html`
+    <div class="box box-success content">
+      <details class="disclosure flow">
+        <summary>Toggle answer</summary>
+        ${content}
+      </details>
+    </div>
   `;
 }
 
@@ -12,8 +14,29 @@ const types = {
   success: "box-success",
   error: "box-error",
 };
+
 function box(content, type = "default") {
   return /*html*/ `<div class="box ${types[type]} content flow">${content}</div>`;
+}
+
+// removes line breaks, tabs, or pairs of spaces
+// quick n dirty way to remove all indentation
+// since Markdown relies on it for formatting
+// https://github.com/11ty/eleventy/issues/402
+function deindent(str) {
+  return str.replace(/\n|\t|\s{2}/g, "").trim();
+}
+
+// need to deindent HTML strings
+// but NOT interpolated content
+// since that needs linebreaks above/below
+// so Markdown will process it lol
+function html(strings, ...vars) {
+  return strings
+    .map((s, i) => {
+      return deindent(s) + (vars[i] || "");
+    })
+    .join("");
 }
 
 module.exports = { solution, box };
