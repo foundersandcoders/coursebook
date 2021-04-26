007b42559d130e089983e6c25a29dd17a973f189
oliverjam <oliverjamesphillips@gmail.com>
2021-04-26 09:24:43 +0100

    Remove unnecessary stuff from cookie starter

--- src/workshops/cookie-auth/starter-files/workshop/server.js
+++ src/workshops/cookie-auth/starter-files/workshop/server.js
@@ -1,20 +1,9 @@
-const crypto = require("crypto");
 const express = require("express");
 
 const server = express();
 
 server.get("/", (request, response) => {
   response.send("<h1>Hello</h1>");
-});
-
-server.get("/login", (request, response) => {
-  console.log("Logging in");
-  response.redirect("/");
-});
-
-server.get("/logout", (request, response) => {
-  console.log("Logging out");
-  response.redirect("/");
 });
 
 const PORT = process.env.PORT || 3000;
