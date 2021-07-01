a24dcef26ee8f0d94ddcb3faa23c00ddd47d702a
Gregor <gregorlthomson@sky.com>
2021-07-01 15:32:30 +0100

    Add Week 5

--- src/_includes/course.11ty.js
+++ src/_includes/course.11ty.js
@@ -41,7 +41,7 @@ exports.render = ({ page: { url }, content }) => {
           <li><${Link} url=${url} href="/course/syllabus/pre-app-1/schedule/">Pre-apprenticeship 1</${Link}></li>
           <li><${Link} url=${url} href="/course/syllabus/pre-app-2/schedule/">Pre-apprenticeship 2</${Link}></li>
           <li><${Link} url=${url} href="/course/syllabus/pre-app-3/schedule/">Pre-apprenticeship 3</${Link}></li>
-          <li><${Link} url=${url} href="/course/syllabus/precourse-4/schedule/">Pre-course 4</${Link}></li>
+          <li><${Link} url=${url} href="/course/syllabus/pre-app-4/schedule/">Pre-apprenticeship 4</${Link}></li>
           <li><${Link} url=${url} href="/course/syllabus/teamwork/schedule/">Teamwork</${Link}></li>
           <li><${Link} url=${url} href="/course/syllabus/http/schedule/">HTTP</${Link}></li>
           <li><${Link} url=${url} href="/course/syllabus/testing/schedule/">Testing</${Link}></li>
---
+++ src/course/syllabus/pre-app-4/schedule.md
@@ -0,0 +1,17 @@
+## Execute Program
+
+Continue working on all the modules you've started.
+
+## Git and GitHub
+
+Continue to expand your learning repository and keep committing at least once a day.
+
+## Linting and formatting
+
+Install [Prettier](https://www.digitalocean.com/community/tutorials/how-to-format-code-with-prettier-in-visual-studio-code) and [ESLint](https://eslint.org/docs/user-guide/getting-started). Ensure both are running by default and keep a consistent coding standard across your project.
+
+## Project
+
+Your [project](../project) this week is to work together with your assigned partner on a set of user stories. We've estimated how long each one should take.
+
+Focus on reading and reviewing each others' code before merging pull requests. Reflect on the challenges of working together and what you've learnt from pairing on a project asynchronously.
