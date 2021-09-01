081576dcff2edd6cc8d8d53636836fc9cbf4f7a8
oliverjam <oliverjamesphillips@gmail.com>
2021-09-01 17:18:52 +0100

    Update TDD array methods explanation

--- src/workshops/tdd-array-methods/starter-files/solution/tdd-explanation.js
+++ src/workshops/tdd-array-methods/starter-files/solution/tdd-explanation.js
@@ -4,23 +4,35 @@
 // if you want to run this code you need to uncomment the <script> tag in index.html
 
 // RED: a failing test
-test("TDD explanation: map() should return an array with the same number of elements", () => {
-  const result = map([1], () => {});
-  equal(result.length, 1);
+test("map() requires array and fn arguments", () => {
+  equal(map(), "Please pass an array");
+  equal(map([]), "Please pass a mapping function");
 });
 
-// to pass our test we need to return an array with the same length
-// the simplest solution is to return the input array
-
-// GREEN: the test now passes
-// function map(array) {
-//   return array;
+// function map(array, fn) {
+//   if (!Array.isArray(array)) return "Please pass an array";
+//   if (typeof fn !== "function") return "Please pass a mapping function";
 // }
 
-// REFACTOR: there's not really any change we can make to improve/simplify this
+// RED: a failing test
+test("map() should return an array", () => {
+  const result = map([], () => {});
+  equal(Array.isArray(result), true);
+});
+
+// GREEN: make the test pass
+// the simplest solution is to just return an empty array
+// function map(array, fn) {
+//   if (!Array.isArray(array)) return "Please pass an array";
+//   if (typeof fn !== "function") return "Please pass a mapping function";
+//   const result = [];
+//   return result;
+// }
+
+// REFACTOR: there's no real way to improve this yet
 
 // RED: a failing test
-test("TDD explanation: map() should transform a one element array using the fn argument", () => {
+test("map() should transform an element using the fn argument", () => {
   const result = map([1], (x) => x + 1);
   equal(result[0], 2);
 });
@@ -29,17 +41,20 @@ test("TDD explanation: map() should transform a one element array using the fn a
 // the simplest solution is to pass the array item in to the function
 // then return a new array with the transformed value in it
 
-//   function map(array, fn) {
-//     const item = array[0];
-//     const mappedItem = fn(item);
-//     const result = [mappedItem];
-//     return result;
-//   }
+// function map(array, fn) {
+//   if (!Array.isArray(array)) return "Please pass an array";
+//   if (typeof fn !== "function") return "Please pass a mapping function";
+//   const result = [];
+//   const item = array[0];
+//   const mappedItem = fn(item);
+//   result.push(mappedItem);
+//   return result;
+// }
 
 // REFACTOR: there's not really any change we can make to improve/simplify this
 
 // RED: a failing test
-test("TDD explanation: map() should transform a two element array using the fn argument", () => {
+test("map() should transform 2 elements using the fn argument", () => {
   const result = map([1, 2], (x) => x * 2);
   equal(result[0], 2);
   equal(result[1], 4);
@@ -49,12 +64,17 @@ test("TDD explanation: map() should transform a two element array using the fn a
 // we can use the same technique we used for the first element
 
 // GREEN: the test now passes
+
 // function map(array, fn) {
-//   const item = array[0];
-//   const mappedItem = fn(item1);
+//   if (!Array.isArray(array)) return "Please pass an array";
+//   if (typeof fn !== "function") return "Please pass a mapping function";
+//   const result = [];
+//   const item1 = array[0];
+//   const mappedItem1 = fn(item1);
+//   result.push(mappedItem1);
 //   const item2 = array[1];
 //   const mappedItem2 = fn(item2);
-//   const result = [mappedItem, mappedItem2];
+//   result.push(mappedItem2);
 //   return result;
 // }
 
@@ -64,7 +84,9 @@ test("TDD explanation: map() should transform a two element array using the fn a
 // 3. put it into a new array
 
 function map(array, fn) {
-  let result = [];
+  if (!Array.isArray(array)) return "Please pass an array";
+  if (typeof fn !== "function") return "Please pass a mapping function";
+  const result = [];
   for (let i = 0; i < array.length; i++) {
     const item = array[i];
     const mappedItem = fn(item);
@@ -74,14 +96,9 @@ function map(array, fn) {
 }
 
 // we probably want another test to make sure this refactor does work for more than 2 item arrays
-test("TDD explanation: map() should transform every item in an array using the fn argument", () => {
+test("map() should transform every item in an array using the fn argument", () => {
   const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const result = map(input, (x) => x * 2);
+  equal(result[8], 18);
   equal(result[9], 20);
 });
-
-// we also haven't considered edge-cases like:
-// 1. What happens if we don't pass an array?
-// 1. What happens if we pass an empty array?
-// 1. What happens if we don't pass a callback?
-// it would be a good idea to test these to force yourself to consider them
