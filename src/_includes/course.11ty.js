2998368c27702caa27551f383e2e03222f9e9372
reubengt <reubengeorgethomas@gmail.com>
2022-02-01 17:04:12 +0000

    remove coaching groups and pair programming workshop links from sidebar

--- src/_includes/course.11ty.js
+++ src/_includes/course.11ty.js
@@ -24,7 +24,6 @@ exports.render = ({ page: { url }, content }) => {
           <li><${Link} url=${url} href="/course/handbook/installation/">Installation Guide</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/course-rules/">Course Rules</${Link}></li>
            <li><${Link} url=${url} href="/course/handbook/user-manuals/">User manuals</${Link}></li>
-          <li><${Link} url=${url} href="/course/handbook/pair-programming/">Pair programming</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/debugging/">Debugging</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/asking-for-help/">Asking for help</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/spikes/">Tech Spikes</${Link}></li>
@@ -37,7 +36,6 @@ exports.render = ({ page: { url }, content }) => {
           <li><${Link} url=${url} href="/course/handbook/retrospectives/">Retrospectives</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/mentor-guidance/">Mentoring guidance</${Link}></li>
           <li><${Link} url=${url} href="/course/handbook/mentor-roles/">What to expect of mentors</${Link}></li>
-          <li><${Link} url=${url} href="/course/handbook/coaching-groups/">Coaching groups</${Link}></li>
           <!-- <li><${Link} url=${url} href="/course/handbook/one-day-projects/">One Day Projects</${Link}></li> -->
           <li><${Link} url=${url} href="/course/handbook/user-research/">User Research</${Link}></li>
         </${Section}>
